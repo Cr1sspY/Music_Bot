c92420dbfb6c97dbafe014f2a2b9a9993f31b401
Cr1sspY <sentluiz14@gmail.com>
2024-03-09 04:41:42 +0300

    added some null func

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconne
 bot = commands.Bot(command_prefix='-', intents=intents)
 
 
-@bot.command()  # Вызывается командой -play
+@bot.command()  # Вызывается командой -play и коннектит бота в канал
 async def play(ctx, url):
     vc = await ctx.message.author.voice.channel.connect()   # Подключаем бота к голосовому каналу
 
@@ -23,21 +23,36 @@ async def play(ctx, url):
             info = ydl.extract_info(url, download=False)    # Если на вход получили ссылку на YouTube
         else:
             info = ydl.extract_info(f"ytsearch:{url}", download=False,)['entries'][0]   # Если на вход получили название
-    await ctx.send('>>> Сейчас играет: \n' + info['title'])     # Вывод сообщения с названием видео
+    await ctx.send('>>> Сейчас играет: \n```' + info['title'] + '```')     # Вывод сообщения с названием видео
     link = info['formats'][0]['url']    # Передаём данные из info
 
     vc.play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=link, **FFMPEG_OPTIONS))    # Воспроизведение
 
 
-@bot.command()  # Вызывается командой -exit
+@bot.command()  # Вызывается командой -exit и отключает бота от канала
 async def exit(ctx):
     await ctx.guild.voice_client.disconnect()   # Отключаем бота от голосового канала
     await ctx.send('Я вышел из канала.')
 
 
-@bot.command()  # Вызывается командой -h
+@bot.command()  # Вызывается командой -h и показывает список команд
 async def h(ctx):
-    await ctx.send('```-play — Включает музыку по ссылке или названию (YouTube only) \n-exit — Выход из голосового канала ```')
+    await ctx.send('```-play — Включает музыку по ссылке или названию (YouTube only) \n-exit — Выход из голосового канала \n-q — Показывает очередь [in dev]\n-pause — Ставит воспроизведение на паузу [in dev]\n-clear — Очищает очередь [in dev]```')
+
+
+@bot.command()  # Вызывается командой -q и показывает очередь
+async def q(ctx):
+    await ctx.send('Показываю очередь. [in dev]')
+
+
+@bot.command()  # Вызывается командой -pause и ставит трек на паузу
+async def pause(ctx):
+    await ctx.send('Ставлю музыку на паузу. [in dev]')
+
+
+@bot.command()  # Вызывается командой -clear и очищает очередь
+async def clear(ctx):
+    await ctx.send('Очищаю очередь. [in dev]')
 
 
 bot.run(config.token)
